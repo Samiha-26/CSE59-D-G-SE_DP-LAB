5ec01c4b26f2621a11033244de1bdbc4cfb63d9a
Samiha-26 <158016366+Samiha-26@users.noreply.github.com>
2026-06-18 16:34:51 +0600

    Update Main7.java

--- Task 2/Main7.java
+++ Task 2/Main7.java
@@ -1,1 +1,21 @@
+interface Clickable {
+	void onClick();
+}
+
+class Button implements Clickable {
+	
+	
+	public void onClick() {
+		System.out.println("Button was Clicked!");
+	}
+}
+public class Main7 {
+
+	public static void main(String[] args) {
+		Button button = new Button ();
+		button.onClick();
+
+	}
+
+}
 
